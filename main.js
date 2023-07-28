3ec87a4b1be337aa347946ba6848a49bffbbf8a1
RichardENeumann <RichardNeumann@googlemail.com>
2023-07-28 13:32:25 +0200

    Implement fixed size grid container

--- main.js
+++ main.js
@@ -3,16 +3,16 @@
 const displayArea = document.querySelector("main");
 
 function buildGrid(gridSize) {
-    for (let i = 0; i < gridSize / 2; i++) {
+    for (let i = 0; i < gridSize; i++) {
         let gridRow = document.createElement("div");
         gridRow.classList.add("gridRow");
         displayArea.appendChild(gridRow);
-        for (let j = 0; j < gridSize / 2; j++) {
+        for (let j = 0; j < gridSize; j++) {
             let gridItem = document.createElement("div");
-            gridItem.classList.add("gridItem");
-            gridItem.style.height = "10px";
-            gridItem.style.width = "10px";
-            gridItem.addEventListener("mouseenter", changeColor);
+                gridItem.classList.add("gridItem");
+                gridItem.style.width = (displayArea.clientWidth / gridSize) + "px";
+                gridItem.style.height = (displayArea.clientHeight / gridSize) + "px";
+                gridItem.addEventListener("mouseenter", changeColor);
             displayArea.lastElementChild.appendChild(gridItem);
         }
     }
@@ -22,5 +22,4 @@ function changeColor(e) {
     e.target.style.backgroundColor = "black";
 }
 
-
-buildGrid(32);
+buildGrid(16);
