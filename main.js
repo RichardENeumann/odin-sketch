910ef143bab55d59f005532ff73b6ad54f04ef82
RichardENeumann <RichardNeumann@googlemail.com>
2023-07-28 10:07:27 +0200

    Build static grid function

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ const displayArea = document.querySelector("main");
 
 function buildGrid(gridSize) {
     for (let i = 0; i < gridSize / 2; i++) {
-        let gridItem = document.createElement("div");
-        gridItem.classList.add("gridRow");
-        gridItem.textContent = "lol";
-        displayArea.appendChild(gridItem);
+        let gridRow = document.createElement("div");
+        gridRow.classList.add("gridRow");
+        displayArea.appendChild(gridRow);
+        for (let j = 0; j < gridSize / 2; j++) {
+            let gridItem = document.createElement("div");
+            gridItem.classList.add("gridItem");
+            gridItem.style.height = "10px";
+            gridItem.style.width = "10px";
+            displayArea.lastElementChild.appendChild(gridItem);
+        }
     }
 
 }
